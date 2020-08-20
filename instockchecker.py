c4bc9297649a6e3eee1d6789332cfa2a87a19095
Zach Osman <zosman@gmu.edu>
2020-08-20 14:29:36 -0400

    syntax fixes

--- instockchecker.py
+++ instockchecker.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python3
+
 import requests
 import json
 import os
 
 
 def main(data):
-    for website in data.websites:
-        html = requests.get(website.url)
-        if website.searchPhrase in html:
-            os.system("echo " + website.notificationMessage +
-                      " | mail " + data.notificationInfo.email)
+    for website in data["websites"]:
+        html = requests.get(website["url"])
+        if website["searchPhrase"] in html:
+            os.system("echo " + website["notificationMessage"] +
+                      " | mail " + data["notificationInfo"]["email"])
+            print(website["notificationMessage"])
 
 
 with open("config.json") as json_data_file:
     data = json.load(json_data_file)
-bar(data)
+print(data)
+main(data)
